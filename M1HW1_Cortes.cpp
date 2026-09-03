98824701b28bf93b681d7029deab951723e9f0d6
kellycortes0220 <cortesok1342@student.faytechcc.edu>
2026-09-03 04:31:07 +0000

    Finished M1HW1

---
+++ M1HW1_Cortes.cpp
@@ -0,0 +1,36 @@
+// CSC 134
+// M1HW1
+// Kelly Cortes Ortega
+// 09/3/2026
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    // This program will describe a movie
+    // The movie's name
+    string name = "Everything, Everywhere, all at once";
+    // The year that the movie was released
+    int year = 2022;
+    // Worldwide gross that the movie made
+    double worldwideGross = 147;
+
+    // print all the information about the movie
+    cout << "The movie " << name;
+    cout << " came out in " << year;
+    cout << ", and had a worldwide gross of around " << worldwideGross;
+    cout << " million dollars." << endl;
+
+    cout << name;
+    cout << " is a " << year;
+    cout << " comedy-drama film about a stressed laundromat owner who must jump through parallel universes to save the existence of everything." << endl;
+
+    cout << "Wang said, \"In another life, I would have really liked.. just doing laundry and taxes with you.\" and walked away.";
+}
+
+
+
+
+
+
+
